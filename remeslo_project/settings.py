aa818762b1f76ad6aef71aa529ba3d3e6d7e1726
Alexander-05-20 <Abeznosikov7@gmail.com>
2026-04-01 00:21:32 +0300

    Fix: исправление ошибок

--- remeslo_project/settings.py
+++ remeslo_project/settings.py
@@ -166,8 +166,8 @@ CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default = '' , cast=Csv())
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST', default='://gmail.com')
-EMAIL_PORT = config('EMAIL_PORT', default=465, cast=int)
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
