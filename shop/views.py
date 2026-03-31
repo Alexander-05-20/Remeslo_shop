606ac34b0627548d66982e729a2fb5a2e6f1bdad
Alexander-05-20 <Abeznosikov7@gmail.com>
2026-04-01 01:32:47 +0300

    Fix: исправление ошибок

--- shop/views.py
+++ shop/views.py
@@ -16,6 +16,7 @@ from django.contrib.auth.models import User
 from django.core.mail import send_mail
 import socket
 import requests
+from decouple import config
 
 
 # Загружает все товары (Product.objects.all()) и показывает их на главной странице.
