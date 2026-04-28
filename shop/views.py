b04643c719c158234cea935243f0fcaeaa984753
Alexander-05-20 <Abeznosikov7@gmail.com>
2026-04-28 23:30:09 +0300

    Fix: исправление ошибок

--- shop/views.py
+++ shop/views.py
@@ -321,13 +321,16 @@ def buy_all_in_cart(request):
         product.save()
 
         # Формируем сообщение
+        tg_message = (
+            f"<b>✅ Новый заказ</b>\n\n"
+            f"👤 <b>Покупатель:</b> {request.user.username}\n"
+            f"📧 <b>Email:</b> {request.user.email}\n"
+            f"📦 <b>Товар:</b> {product.name}\n"
+            f"🔢 <b>Количество:</b> {requested_quantity} шт.\n"
+            f"📝 <b>Остаток на складе:</b> {product.stock} шт.\n"
+        )
         full_price = product.price * requested_quantity
-        messages.success(request, f"Покупатель: {request.user.username}\n"
-                          f"Email: {request.user.email}\n"
-                          f"Товар: {product.name}\n"
-                          f"Количество: {requested_quantity}\n"
-                          f"Остаток на складе: {product.stock}\n"
-                          f"Общая сумма: {full_price} ₽")
+        tg_message =(f"💰 <b>Итого:</b> {full_price} ₽")
 
     # Удаляем товары из корзины
     cart_items.delete()
